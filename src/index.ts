0e5ce6a64c26dd7d00b138466e3cd70e1fef4d89
root <google@google.com>
2023-11-17 14:27:52 +0000

    Added the HTTP server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
+if (!process.env.TOKEN) {
+    console.log("Please create a .env file and fill in your token. Read the README.md to learn how to get your token.");
+    process.exit(-1);
+}
 
 import Discord from "./discord/discord";
 import IDownloadLinkData from "./types/IDownloadLinkData";
@@ -10,15 +14,16 @@ function parseDownloadLink(link: string): IDownloadLinkData {
     const parts = link.split("/");
 
     return {
-	channel_id: parts[4],
-	message_id: parts[5],
-	file_name: parts[6] 
+	channel_id: parts[4] || "",
+	message_id: parts[5] || "",
+	file_name: parts[6] || "" 
     };
 }
 
 async function getFullLink(link: string) {
-    const download_link_data = (parseDownloadLink(link));
-    return await Discord.getDownloadLink(download_link_data.channel_id, download_link_data.message_id, download_link_data.file_name);
+    const data = (parseDownloadLink(link));
+    if (!data.channel_id || !data.file_name || !data.message_id) return "";
+    return await Discord.getDownloadLink(data.channel_id, data.message_id, data.file_name);
 }
 
 export { parseDownloadLink, Discord, getFullLink };
