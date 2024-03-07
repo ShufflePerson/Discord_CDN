85a66e7153622397184d57b4dc636ec14a101a2d
Shuffle <121307743+ShufflePerson@users.noreply.github.com>
2024-03-07 18:26:26 +0200

    Update index.ts
    Using the refresh-urls API endpoint now

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
+import axios from "axios"
+
 if (!process.env.TOKEN) {
     console.log("Please create a .env file and fill in your token. Read the README.md to learn how to get your token.");
     process.exit(-1);
@@ -9,31 +11,35 @@ if (!process.env.TOKEN) {
 import Discord from "./discord/discord";
 import IDownloadLinkData from "./types/IDownloadLinkData";
 
-
 function parseDownloadLink(link: string): IDownloadLinkData {
     if (link.includes("?")) {
-	link = link.split("?")[0];
+        link = link.split("?")[0];
     }
 
     const parts = link.split("/");
     let offset = 0;
     if (parts[6])
-	offset++;
+        offset++;
 
     return {
-	channel_id: parts	[3 	 + offset] || "",
-	message_id: parts	[4  	 + offset] || "",
-	file_name: parts	[5   	 + offset] || "" 
+        channel_id: parts[3 + offset] || "",
+        message_id: parts[4 + offset] || "",
+        file_name: parts[5 + offset] || ""
     };
 }
 
 async function getFullLink(link: string) {
-    const data = (parseDownloadLink(link));
-    if (!data.channel_id || !data.file_name || !data.message_id) {
-	console.log("Missing one of the values: ", data);
-	return "";
+    try {
+        const { data } = await axios.post("https://discord.com/api/v9/attachments/refresh-urls", {"attachment_urls":[link]}, {
+            headers: {
+                "Authorization": process.env.TOKEN,
+            }
+        });
+
+        return data.refreshed_urls[0].refreshed;
+    } catch (ex) {
+        return "";
     }
-    return await Discord.getDownloadLink(data.channel_id, data.message_id, data.file_name);
 }
 
 export { parseDownloadLink, Discord, getFullLink };
