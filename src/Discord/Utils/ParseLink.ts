f01a5d6c314d8aa625c06d7ac06c4e35b83851b5
Shuffle <121307743+ShufflePerson@users.noreply.github.com>
2024-03-12 14:09:06 +0200

    Implemented the `ParseLink` function

---
+++ src/Discord/Utils/ParseLink.ts
@@ -0,0 +1,36 @@
+import ELinkIssue from "../Types/ELinkIssue";
+import ILinkData from "../Types/ILinkData";
+import IParsedLink from "../Types/IParsedLink";
+
+
+/**
+ * Ensures the given `input` is in the right format so its possible to extract data from it. 
+ * @param input Discord Download link given in any format.  
+ * @returns A IParsedLink
+ */
+function ParseLink(input: string): IParsedLink {
+    if (input.includes("?"))
+        input = input.split("?")[0];
+
+    if (input.includes("attachments/"))
+        input = input.split("attachments/")[1];
+
+    let slashParts = input.split("/");
+    if (slashParts.length != 3)
+        return { error: ELinkIssue.INVALID_SLASH_AMOUNT };
+
+    const [ channelID, fileID, fileName ] = slashParts;
+
+    if (isNaN(Number(channelID)))
+        return { error: ELinkIssue.CHANNEL_ID_NAN};
+
+    if (isNaN(Number(fileID)))
+        return { error: ELinkIssue.FILE_ID_NAN};
+
+    if (!fileName.includes("."))
+        return { error: ELinkIssue.FILENAME_NO_DOT };
+
+}
+
+
+export default ParseLink;
