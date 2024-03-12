9d7bd1e343ff90ee42dc73c3e2cbe0b912ecfbf3
Shuffle <121307743+ShufflePerson@users.noreply.github.com>
2024-03-12 13:42:34 +0200

    Added `GetUTF8Base64` func

---
+++ src/Discord/Utils/GetUTF8Base64.ts
@@ -0,0 +1,23 @@
+function hasNonUTF8Characters(str: string): boolean {
+  return /[^\x00-\x7F]/.test(str);
+}
+
+
+/**
+ * Returns a `boolean` whetever the given string is a valid UTF-8 string when decoded from Base64
+ * @param input The Base64 String to Check
+ */
+function GetUTF8Base64(input: string): string | null {
+    try {
+        const buf = Buffer.from(input, "base64");
+        const decodedBase64 = buf.toString();
+        if (hasNonUTF8Characters(decodedBase64))
+            throw new Error("Contains Non UTF-8 character(s).")
+
+        return decodedBase64;
+    } catch (ex) {
+        return null;
+    }
+}
+
+export default GetUTF8Base64;
